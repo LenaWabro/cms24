87c0c5cc32d3727056a7bbe33e73244720fc3b06
lenaw <s2310456027@fhooe.at>
2024-12-23 20:57:47 +0100

    versuche, lightbox zu fixen( nichts verändert)

--- packages/kwm24/Resources/Public/JavaScript/Src/main.js
+++ packages/kwm24/Resources/Public/JavaScript/Src/main.js
@@ -1,1 +1,6 @@
 console.log('WE LOVE TYPO3');
+console.log("main.js actions!");
+document.addEventListener('DOMContentLoaded', function () {
+    Fancybox.bind('[data-fancybox="gallery"]', {
+    });
+});
