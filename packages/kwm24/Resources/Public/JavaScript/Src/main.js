e4f4b2bef9560a4332199e58c9c6ab3b1bca00c3
lenaw <s2310456027@fhooe.at>
2024-12-30 20:38:15 +0100

    main.js auskommentiert

--- packages/kwm24/Resources/Public/JavaScript/Src/main.js
+++ packages/kwm24/Resources/Public/JavaScript/Src/main.js
@@ -1,7 +1,16 @@
-console.log('WE LOVE TYPO3');
-console.log("main.js actions!");
+console.log("main.js actions!")
+
+/*
+console.log("jQuery Version:", typeof jQuery !== 'undefined' ? jQuery.fn.jquery : "jQuery is not loaded!");
+
 document.addEventListener('DOMContentLoaded', function () {
     Fancybox.bind('[data-fancybox="gallery"]', {
-
+        buttons: ["close"],
+        caption: function (instance, item) {
+            return $(this).data('caption') || '';
+        }
     });
 });
+
+ */
+
