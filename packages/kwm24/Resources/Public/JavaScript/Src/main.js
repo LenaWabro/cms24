c8d46863e8bf801109ed7e7e2511e209b77bf02a
lenaw <s2310456027@fhooe.at>
2025-01-14 20:33:41 +0100

    downloadbutton- im eienen spakt er oft

--- packages/kwm24/Resources/Public/JavaScript/Src/main.js
+++ packages/kwm24/Resources/Public/JavaScript/Src/main.js
@@ -1,5 +1,6 @@
 console.log("main.js actions!")
 
+//Fancybox einbinden
 document.addEventListener('DOMContentLoaded', function () {
     Fancybox.bind('[data-fancybox="gallery"]', {
         buttons: ["close"],
@@ -8,6 +9,22 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+
+//Download für CV
+document.getElementsByClassName('btn btn-info')[0].addEventListener('click', function () {
+    const link = document.createElement('a');
+    link.href = '/fileadmin/Website/Wabro-Lena-CV.pdf'; // Pfad zur PDF-Datei
+    link.download = 'CV_Lena Wabro'; // Name der heruntergeladenen Datei
+    link.click();
+});
+
+
+
+
+
+
+
 
 
 
